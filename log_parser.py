9d1fd86f98afb98d0dfebf9d111107ac3a05b47d
mahar000 <m_khubaib@yahoo.com>
2022-08-07 00:14:32 -0400

    Update log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -6,40 +6,32 @@ file format is
 string1,string2,string3
 
 script will generate grep  as follows
-grep -E 'string1' what_ever.csv|grep -E  'string2'|grep -E 'string3'
+grep -E 'string1' RTC.csv|grep -E  'string2'|grep -E 'string3'
 
 '''
 
 from datetime import datetime, timedelta
 import subprocess
 import csv
-
-final_result = "/what_ever/log/result_rtc.log"
-File = "/what_ever/log/what_ever.log"
-result_file = "/what_ever/scripts/what_ever.csv"
-string_file = "/what_ever/scripts/mystrings_rtc.csv"
-
+import pytz
 
 def get_log():
     '''
     Function to get last 5 minutes log
     '''
-    now = datetime.now()
+    now = datetime.now(pytz.timezone('US/Pacific'))
     print(now)
     lookback = timedelta(minutes=5)
     print(lookback)
     five_min_before = (now - lookback).strftime("%b %e %H:%M:%S")
+    # five_min_before = (now - lookback).strftime("%m/%d/%Y %H:%M:%S")
     print(five_min_before)
-
-    file_object = open(result_file, 'w')
-    f = open(File, "r")
-    for line in f:
-        if line[:14] > five_min_before:
-            # print(line)
-            file_object.write(line)
-
-    f.close()
-    file_object.close()
+    with open(result_file, 'w') as file_object:
+        with open(File, 'r') as f:
+            for line in f:
+                if line[:14] > five_min_before:
+                    # print(line)
+                    file_object.write(line)
 
 
 def get_files():
@@ -47,12 +39,12 @@ def get_files():
     This function is not used yet, we can use it in the Future,
     if we need to generate any stats using pandas
     '''
-    subprocess.call("awk -F: '{print NF}' what_ever.csv |sort -nu|\
+    subprocess.call("awk -F: '{print NF}' RTC.csv |sort -nu|\
     while read line;\
-    do awk -F: '(NF=='$line') {print }' what_ever.csv  >what_ever_${line}.csv;done ",
+    do awk -F: '(NF=='$line') {print }' RTC.csv  >RTC_${line}.csv;done ",
                     shell=True)
 
-    cmd = "ls what_ever_*.csv"
+    cmd = "ls RTC_*.csv"
     output = subprocess.run(cmd, shell=1, stdout=subprocess.PIPE).stdout.decode('utf-8')
 
     file_list = output.split()
@@ -64,46 +56,62 @@ def get_results():
     function to  get the grep command
     '''
 
-    f1 = open(final_result, "a+")
-    csv_file = open(string_file, "r")
-    csv_reader = csv.reader(csv_file, delimiter=',')
+    with open(final_result, "a+") as f1:
+        with open(string_file, "r", encoding="utf-8") as  csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                myarray1 = row
+                myarray = list(filter(None, myarray1))
+                # print(myarray)
 
-    for row in csv_reader:
-        myarray1 = row
-        myarray = list(filter(None, myarray1))
+                if len(myarray) == 1:
+                    str = f"{myarray[0]} {result_file} "
+                    print(str)
+                    cmd = str
+                    subprocess.run([cmd], shell=True, stdout=f1)
 
-        print(len(myarray))
-        i = 0
-
-        for n in range(len(myarray)):
-            if i == 0:
-                stri = myarray[n]
-                print(i)
-                i = i + 1
-                str0 = " grep -E "
-                print(str0)
-                str1 = stri
-
-                str3 = f"{str0} '{str1}'  {result_file}"
-                str1 = str3
-                print(str1)
-                cmd = str1
-              
-                subprocess.run([cmd], shell=True, stdout=f1)
-            elif i > 0:
-                stri = myarray[n]
-                str2 = " | grep -E " + f"'{stri}'"
-                str3 = f"{str1} {str2}"
-                str1 = str3
-                i = i + 1
-                print(str1)
-                cmd = str1
-                subprocess.run([cmd], shell=True, stdout=f1)
-
-    csv_file.close()
-    f1.close()
+                elif len(myarray) > 1:
+                    i = 0
+                    y = len(myarray)
+                    # print(y)
+                    for item in myarray:
+                        if i == 0:
+                            str1 = f"{item} {result_file}"
+                            str = str1
+                            # print(str)
+                            i = i + 1
+                        elif i >= 1:
+                            str1 = f"{str} {item}"
+                            i = i + 1
+                            print(i)
+                            str = str1
+                            if i == len(myarray):
+                                print(str)
+                                cmd = str
+                                subprocess.run([cmd], shell=True, stdout=f1)
 
 
 # Running the functions
-get_log()
-get_results()
+File1 = "/blah/scripts/INPUT/input.txt"
+File2 = "/blah/scripts/INPUT/dir.txt"
+
+with open(File1, 'r', encoding="utf-8") as t1:
+    fileone = t1.readlines()
+    # fileone = [item.strip(\n) for item in fileone]
+    fileone = list(map(str.strip, fileone))
+    print(fileone)
+
+with open(File2, 'r', encoding="utf-8") as t2:
+    filetwo = t2.readlines()
+    # fileone = [item.strip(\n) for item in fileone]
+    filetwo = list(map(str.strip, filetwo))
+    print(filetwo)
+
+for item in fileone:
+    File = filetwo[1] + item
+    final_result = filetwo[1] + "result_" + item.split(".")[0].lower() + ".log"
+    result_file = filetwo[0] + item.split(".")[0] + ".csv"
+    string_file = filetwo[0] + "mystrings_" + item.split(".")[0].lower() + ".csv"
+    print(File, final_result, result_file, string_file)
+    get_log()
+    get_results()
