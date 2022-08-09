09b5d4d76946c7b9cb4af9eb2076e968ea9c112a
mahar000 <m_khubaib@yahoo.com>
2022-08-09 11:42:06 -0400

    Create parse_logs_sed.py

---
+++ parse_logs_sed.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+'''
+Script to parse smf log files , script is using config file in csv format,
+the config file is  mystrings.csv
+file format is
+string1,string2,string3
+
+script will generate grep  as follows
+grep -E 'string1' RTC.csv|grep -E  'string2'|grep -E 'string3'
+
+'''
+
+from datetime import datetime, timedelta
+import subprocess
+import csv
+
+
+def get_log():
+    '''
+    Function to get last 5 minutes log using sed 
+    '''
+    from datetime import datetime, timedelta
+    now = datetime.now()
+    now1 = datetime.now().strftime("%m\/%d\/%Y %H:%M:%S")
+    lookback = timedelta(minutes=5)
+    five_min_before = (now - lookback).strftime("%m\/%d\/%Y %H:%M:%S")
+    cmd = f'''sed -n "/{five_min_before}/,/{now1}/p"   {File}'''
+    print(cmd)
+    #subprocess.run([cmd], shell=True, stdout=result_file)
+    with open(result_file, 'w') as file_object:
+        subprocess.run([cmd], shell=True, stdout=file_object)
+
+
+def get_files():
+    '''
+    This function is not used yet, we can use it in the Future,
+    if we need to generate any stats using pandas
+    '''
+    subprocess.call("awk -F: '{print NF}' RTC.csv |sort -nu|\
+    while read line;\
+    do awk -F: '(NF=='$line') {print }' RTC.csv  >RTC_${line}.csv;done ",
+                    shell=True)
+
+    cmd = "ls RTC_*.csv"
+    output = subprocess.run(cmd, shell=1, stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+    file_list = output.split()
+    print(file_list)
+
+
+def get_results():
+    '''
+    function to  get the grep command
+    '''
+
+    with open(final_result, "a+") as f1:
+        with open(string_file, "r", encoding="utf-8") as  csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                myarray1 = row
+                myarray = list(filter(None, myarray1))
+                # print(myarray)
+
+                if len(myarray) == 1:
+                    str = f"{myarray[0]} {result_file} "
+                    print(str)
+                    cmd = str
+                    subprocess.run([cmd], shell=True, stdout=f1)
+
+                elif len(myarray) > 1:
+                    i = 0
+                    y = len(myarray)
+                    # print(y)
+                    for item in myarray:
+                        if i == 0:
+                            str1 = f"{item} {result_file}"
+                            str = str1
+                            # print(str)
+                            i = i + 1
+                        elif i >= 1:
+                            str1 = f"{str} {item}"
+                            i = i + 1
+                            print(i)
+                            str = str1
+                            if i == len(myarray):
+                                print(str)
+                                cmd = str
+                                subprocess.run([cmd], shell=True, stdout=f1)
+
+
+# Running the functions
+File1 = "/var/opt/SIU/scripts/INPUT/input.txt"
+File2 = "/var/opt/SIU/scripts/INPUT/dir.txt"
+
+with open(File1, 'r', encoding="utf-8") as t1:
+    fileone = t1.readlines()
+    # fileone = [item.strip(\n) for item in fileone]
+    fileone = list(map(str.strip, fileone))
+    print(fileone)
+
+with open(File2, 'r', encoding="utf-8") as t2:
+    filetwo = t2.readlines()
+    # fileone = [item.strip(\n) for item in fileone]
+    filetwo = list(map(str.strip, filetwo))
+    print(filetwo)
+
+for item in fileone:
+    File = filetwo[1] + item
+    final_result = filetwo[1] + "result_" + item.split(".")[0].lower() + ".log"
+    result_file = filetwo[0] + item.split(".")[0] + ".csv"
+    string_file = filetwo[0] + "mystrings_" + item.split(".")[0].lower() + ".csv"
+    print(File, final_result, result_file, string_file)
+    get_log()
+    get_results()
